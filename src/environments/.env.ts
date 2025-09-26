9d70668bc026dddd493eda1dcc00f19aa7eb71d8
Josephine Kingombe <josephinekingombe11@gmail.com>
2025-09-26 03:32:59 +0100

    login finish

--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -2,8 +2,8 @@
 /* tslint:disable */
 export default {
   'mifos_x': {
-    'version': '250924',
-    'hash': '1a39c666'
+    'version': '250926',
+    'hash': '6360639a'
   },
   'allow_switching_backend_instance': true
 };
